d773e1a2b89f57fbac7cd1522bcc067d139c1d33
Bhanu <bhanu@Bhanus-MacBook-Air.local>
2025-04-08 18:00:12 +0530

    user controller

--- src/app.js
+++ src/app.js
@@ -22,4 +22,12 @@ app.use(express.urlencoded({
 app.use(express.static('public'));
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//routes
+
+import userRouter from './routes/user.router.js';
+
+//routes declaration
+app.use('/api/v1/users', userRouter);
+
 export {app};
